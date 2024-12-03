5db772a8e40cb428e3bf7d50145579ca64e9443d
EmmanuelAdu <christorenzo20@gmail.com>
2024-12-03 17:53:14 +0000

    Apply aggregations and window functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -22,7 +22,7 @@ SELECT
     Property.property_id,
     Property.prop_name,
     COUNT(Booking.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(Booking.booking_id) DESC) AS rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(Booking.booking_id) DESC) AS row_number
 FROM 
     Property
 LEFT JOIN 
@@ -32,4 +32,5 @@ ON
 GROUP BY 
     Property.property_id, Property.prop_name
 ORDER BY 
-    rank;
+    row_number;
+
